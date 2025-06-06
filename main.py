6b07ef17e18e0ca119fb994d8f6885fd06d6518c
MUKENI <mukenigrace0@gmail.com>
2025-06-06 15:29:55 +0300

    6/06/2025

---
+++ main.py
@@ -0,0 +1,10 @@
+print("HELLO,welcome to ROBOT_BARISTA")
+name = input("What is your name?\n ")
+print("hey, " + name + " thank you for coming in today,What would you like to order?")
+menu = "cappucino,black coffee,latte,white coffee,iced coffee and espresso"
+print("This is what we are serving today,\n"
+      + menu)
+order = input()
+print("sounds great, " +  name  + " you're " + order +  " will be  ready in a few,\n"
+      "Thank you very much for visiting ROBOT_BARISTA \n"
+"You are always welcomed.")
